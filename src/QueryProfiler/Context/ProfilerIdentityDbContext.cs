46a24ac6e06b392fe45d81a660c13efbb2e11d22
Furkan GÜNGÖR <furkan.dvlp@gmail.com>
2020-10-10 15:58:50 +0300

    🔨 Add DbContext for IdentityDbContext

---
+++ src/QueryProfiler/Context/ProfilerIdentityDbContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryProfiler.Context
+{
+    public class ProfilerIdentityDbContext<TUser> : IdentityDbContext<TUser>
+        where TUser : IdentityUser
+    {
+        public ProfilerIdentityDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public ProfilerIdentityDbContext()
+        {
+        }
+    }
+
+    public class ProfilerIdentityDbContext<TUser, TRole, TKey> : IdentityDbContext<TUser, TRole, TKey>
+        where TUser : IdentityUser<TKey>
+        where TRole : IdentityRole<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public ProfilerIdentityDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public ProfilerIdentityDbContext()
+        {
+        }
+    }
+
+    public class ProfilerIdentityDbContext<TUser, TRole, TKey, TUserClaim, TUserRole, TUserLogin, TRoleClaim, TUserToken> : IdentityUserContext<TUser, TKey, TUserClaim, TUserLogin, TUserToken>
+        where TUser : IdentityUser<TKey>
+        where TRole : IdentityRole<TKey>
+        where TKey : IEquatable<TKey>
+        where TUserClaim : IdentityUserClaim<TKey>
+        where TUserRole : IdentityUserRole<TKey>
+        where TUserLogin : IdentityUserLogin<TKey>
+        where TRoleClaim : IdentityRoleClaim<TKey>
+        where TUserToken : IdentityUserToken<TKey>
+    {
+        public ProfilerIdentityDbContext()
+        {
+        }
+
+        public ProfilerIdentityDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
