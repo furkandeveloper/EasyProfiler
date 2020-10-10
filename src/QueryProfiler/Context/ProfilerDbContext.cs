4c8d3bfcb5d0eef0046ae6967a28b2815d822b46
Furkan GÜNGÖR <furkan.dvlp@gmail.com>
2020-10-10 15:55:24 +0300

    🔨 Add Profiler DbContext.

---
+++ src/QueryProfiler/Context/ProfilerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryProfiler.Context
+{
+    public class ProfilerDbContext : DbContext
+    {
+        public ProfilerDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public ProfilerDbContext()
+        {
+        }
+    }
+}
