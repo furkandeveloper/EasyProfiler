221232bf938c2f10884e85aa396a7379d4490ff2
Furkan GÜNGÖR <furkan.dvlp@gmail.com>
2020-10-10 16:05:14 +0300

    📝 Add xml comment for Profiler DbContext.

--- src/QueryProfiler/Context/ProfilerIdentityDbContext.cs
+++ src/QueryProfiler/Context/ProfilerIdentityDbContext.cs
@@ -7,6 +7,13 @@ using System.Text;
 
 namespace QueryProfiler.Context
 {
+    /// <summary>
+    /// Profiler Identity DbContext.
+    /// Use this dbcontext if use identity framework.
+    /// </summary>
+    /// <typeparam name="TUser">
+    /// Identity User.
+    /// </typeparam>
     public class ProfilerIdentityDbContext<TUser> : IdentityDbContext<TUser>
         where TUser : IdentityUser
     {
@@ -19,6 +26,18 @@ namespace QueryProfiler.Context
         }
     }
 
+    /// <summary>
+    /// Profiler Identity DbContext.
+    /// Use this dbcontext if use identity framework.</summary>
+    /// <typeparam name="TUser">
+    /// Identity User.
+    /// </typeparam>
+    /// <typeparam name="TRole">
+    /// Identity Role.
+    /// </typeparam>
+    /// <typeparam name="TKey">
+    ///  The type of the primary key for users and roles.
+    /// </typeparam>
     public class ProfilerIdentityDbContext<TUser, TRole, TKey> : IdentityDbContext<TUser, TRole, TKey>
         where TUser : IdentityUser<TKey>
         where TRole : IdentityRole<TKey>
@@ -33,6 +52,33 @@ namespace QueryProfiler.Context
         }
     }
 
+    /// <summary>
+    /// Profiler Identity DbContext.
+    /// Use this dbcontext if use identity framework.</summary>
+    /// <typeparam name="TUser">
+    /// Identity User.
+    /// </typeparam>
+    /// <typeparam name="TRole">
+    /// Identity Role.
+    /// </typeparam>
+    /// <typeparam name="TKey">
+    ///  The type of the primary key for users and roles.
+    /// </typeparam>
+    /// <typeparam name="TUserClaim">
+    /// The type of the user claim object.
+    /// </typeparam>
+    /// <typeparam name="TUserRole">
+    /// The type of the user role object.
+    /// </typeparam>
+    /// <typeparam name="TUserLogin">
+    /// The type of the user login object.
+    /// </typeparam>
+    /// <typeparam name="TRoleClaim">
+    /// The type of the user role claim object.
+    /// </typeparam>
+    /// <typeparam name="TUserToken">
+    /// The type of the user token object.
+    /// </typeparam>
     public class ProfilerIdentityDbContext<TUser, TRole, TKey, TUserClaim, TUserRole, TUserLogin, TRoleClaim, TUserToken> : IdentityUserContext<TUser, TKey, TUserClaim, TUserLogin, TUserToken>
         where TUser : IdentityUser<TKey>
         where TRole : IdentityRole<TKey>
