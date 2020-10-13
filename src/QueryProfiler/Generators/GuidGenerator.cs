4fe9b5ee307116b6beedd0b71bef830efa3dd33d
Furkan GÜNGÖR <furkan.dvlp@gmail.com>
2020-10-13 23:08:44 +0300

    🔨 Add Guid value generator for primary keys.

---
+++ src/QueryProfiler/Generators/GuidGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryProfiler.Generators
+{
+    /// <summary>
+    /// Guid value generator for primary key.
+    /// </summary>
+    public class GuidGenerator : ValueGenerator
+    {
+        public override bool GeneratesTemporaryValues => false;
+
+        protected override object NextValue(EntityEntry entry)
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
