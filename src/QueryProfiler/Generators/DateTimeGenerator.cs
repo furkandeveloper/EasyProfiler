e9121344e3cbef3f167186a06541c48c726cb3e4
Furkan GÜNGÖR <furkan.dvlp@gmail.com>
2020-10-13 23:10:26 +0300

    🔨 Add date time value generator.

---
+++ src/QueryProfiler/Generators/DateTimeGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryProfiler.Generators
+{
+    /// <summary>
+    /// Date time value generator.
+    /// </summary>
+    internal class DateTimeGenerator : ValueGenerator
+    {
+        public override bool GeneratesTemporaryValues => false;
+
+        protected override object NextValue(EntityEntry entry)
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
