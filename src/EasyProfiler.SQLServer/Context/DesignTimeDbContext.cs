9cf54b8a1719f2f73877e5573ba857d801e5884a
Furkan GÜNGÖR <furkan.dvlp@gmail.com>
2020-10-15 21:54:25 +0300

    🔨 Add DesignTimeDbContext.

---
+++ src/EasyProfiler.SQLServer/Context/DesignTimeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyProfiler.SQLServer.Context
+{
+    public class DesignTimeDbContext : IDesignTimeDbContextFactory<ProfilerDbContext>
+    {
+        public ProfilerDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ProfilerDbContext>();
+            
+            return new ProfilerDbContext(optionsBuilder.Options);
+        }
+    }
+}
