3f14c149f2fd737f9b041f840231056eaf96e2f2
Furkan GÜNGÖR <furkan.dvlp@gmail.com>
2020-10-15 21:59:14 +0300

    🔨 Add initial migration.

---
+++ src/EasyProfiler.SQLServer/Migrations/20201015185716_Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EasyProfiler.SQLServer.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Profilers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Query = table.Column<string>(nullable: false),
+                    Duration = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profilers", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profilers_Duration",
+                table: "Profilers",
+                column: "Duration");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Profilers");
+        }
+    }
+}
