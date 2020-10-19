5f28fff780be491ecc157d5023ce700693cb6cf1
furkandeveloper <furkan.dvlp@gmail.com>
2020-10-19 21:44:18 +0300

    🗃 Change duration field type.

---
+++ src/EasyProfiler.SQLServer/Migrations/20201019184147_ChangeDurationFieldType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EasyProfiler.SQLServer.Migrations
+{
+    public partial class ChangeDurationFieldType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "Duration",
+                table: "Profilers",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Duration",
+                table: "Profilers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(TimeSpan));
+        }
+    }
+}
