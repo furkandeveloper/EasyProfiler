3af03f86b49b7bbf9268bf9c726c04ad517a1fc1
furkandeveloper <furkan.dvlp@gmail.com>
2020-10-19 20:55:55 +0300

    🔨 Add interceptors for Easy Profiler.

---
+++ src/EasyProfiler.SQLServer/Interceptors/EasyProfilerInterceptors.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyProfiler.SQLServer.Interceptors
+{
+    public class EasyProfilerInterceptors
+    {
+    }
+}
