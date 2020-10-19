7a38362a988f56185db11d24c5fc02326c969c34
furkandeveloper <furkan.dvlp@gmail.com>
2020-10-19 22:53:38 +0300

    🔨 Add SampleDbContext.

---
+++ samples/EasyProfiler.Web/SampleDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyProfiler.Web
+{
+    public class SampleDbContext : DbContext
+    {
+        public SampleDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected SampleDbContext()
+        {
+        }
+    }
+}
