e4f612b30fcc2f5ebd8e99b95a5efa72dd847b7e
furkandeveloper <furkan.dvlp@gmail.com>
2020-10-22 22:56:35 +0300

    🔨 Add Advanced Filter Model for profiler entity.

---
+++ src/EasyProfiler.SQLServer/Models/AdvancedFilterModel.cs
@@ -0,0 +1,30 @@
+﻿using AutoFilterer.Attributes;
+using AutoFilterer.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyProfiler.SQLServer.Models
+{
+    /// <summary>
+    /// Advanced filter model for profiler entity.
+    /// </summary>
+    [PossibleSortings("Query","Duration")]
+    public class AdvancedFilterModel : PaginationFilterBase
+    {
+        public AdvancedFilterModel()
+        {
+            this.Sort = "Duration";
+            this.SortBy = AutoFilterer.Enums.Sorting.Descending;
+        }
+        /// <summary>
+        /// SQL Query.
+        /// </summary>
+        public string Query { get; set; }
+
+        /// <summary>
+        /// Query duration.
+        /// </summary>
+        public TimeSpan Duration { get; set; }
+    }
+}
