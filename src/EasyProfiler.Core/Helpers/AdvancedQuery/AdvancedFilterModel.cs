cd430ed723e8e3b5960b368a85083dba97f0e52e
furkandeveloper <furkan.dvlp@gmail.com>
2020-10-28 17:39:06 +0300

    🔨 Add AdvancedFilterModel.

---
+++ src/EasyProfiler.Core/Helpers/AdvancedQuery/AdvancedFilterModel.cs
@@ -0,0 +1,32 @@
+﻿using AutoFilterer.Attributes;
+using AutoFilterer.Types;
+using EasyProfiler.Core.Helpers.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyProfiler.Core.Helpers.AdvancedQuery
+{
+    /// <summary>
+    /// Advanced filter model for profiler entity.
+    /// </summary>
+    [PossibleSortings("Query", "Duration")]
+    public class AdvancedFilterModel : PaginationFilterBase
+    {
+        public AdvancedFilterModel()
+        {
+            this.Sort = "Duration";
+            this.SortBy = AutoFilterer.Enums.Sorting.Descending;
+        }
+        /// <summary>
+        /// SQL Query.
+        /// </summary>
+        [ToLowerContainsComparison]
+        public string Query { get; set; }
+
+        /// <summary>
+        /// Query duration.
+        /// </summary>
+        public Range<TimeSpan> Duration { get; set; }
+    }
+}
