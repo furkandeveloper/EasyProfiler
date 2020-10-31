3d395658b854021a7808c5f8de0cd15d0edf43f7
furkandeveloper <furkan.dvlp@gmail.com>
2020-10-31 23:43:50 +0300

    🔨 Implement DbContextb

---
+++ src/EasyProfiler.PostgreSQL/Context/ProfilerDbContext.cs
@@ -0,0 +1,53 @@
+﻿using EasyProfiler.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyProfiler.PostgreSQL.Context
+{
+    /// <summary>
+    /// Profiler DbContext for PostgreSQL
+    /// </summary>
+    public class ProfilerDbContext : DbContext
+    {
+        public ProfilerDbContext(DbContextOptions<ProfilerDbContext> options) : base(options)
+        {
+        }
+
+        protected ProfilerDbContext()
+        {
+        }
+
+        #region Tables
+        public virtual DbSet<Profiler> Profilers { get; set; }
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Profiler>(entity =>
+            {
+                entity
+                    .HasKey(pk => pk.Id);
+
+                entity
+                    .HasIndex(i => i.Duration);
+
+                entity
+                   .Property(p => p.Id)
+                   .HasValueGenerator<GuidGenerator>()
+                   .ValueGeneratedOnAdd();
+
+                entity
+                    .Property(p => p.Query)
+                    .IsRequired();
+
+                entity
+                    .Property(p => p.Duration)
+                    .HasColumnType("time");
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
