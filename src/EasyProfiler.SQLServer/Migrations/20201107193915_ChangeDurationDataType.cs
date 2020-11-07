1c90ab0f57cdf19a69a01d6d3166040c222c4e92
furkandeveloper <furkan.dvlp@gmail.com>
2020-11-07 22:40:10 +0300

    🗃 Add Migration for SQL Server Provider.

---
+++ src/EasyProfiler.SQLServer/Migrations/20201107193915_ChangeDurationDataType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EasyProfiler.SQLServer.Migrations
+{
+    public partial class ChangeDurationDataType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Duration",
+                table: "Profilers",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "Duration",
+                table: "Profilers",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
