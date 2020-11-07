8b0ad61beb4ca848da26d087138d9500d3b17b73
furkandeveloper <furkan.dvlp@gmail.com>
2020-11-07 22:47:24 +0300

    🔥 Remove migration folder for SQL Server.

--- src/EasyProfiler.SQLServer/Migrations/ProfilerDbContextModelSnapshot.cs
+++ src/EasyProfiler.SQLServer/Migrations/ProfilerDbContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace EasyProfiler.SQLServer.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.8")
+                .HasAnnotation("ProductVersion", "3.1.9")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
