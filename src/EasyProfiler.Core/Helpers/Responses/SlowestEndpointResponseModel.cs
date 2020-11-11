d7dff90e85f5b7de11b3ab380b180b55ec71637b
furkandeveloper <furkan.dvlp@gmail.com>
2020-11-12 01:14:15 +0300

    🔨 Add Slowest Endpoint Response Modelb

---
+++ src/EasyProfiler.Core/Helpers/Responses/SlowestEndpointResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyProfiler.Core.Helpers.Responses
+{
+    public class SlowestEndpointResponseModel
+    {
+        public string RequestUrl { get; set; }
+
+        public int Count { get; set; }
+
+        public TimeSpan AvarageDurationTime { get; set; }
+    }
+}
