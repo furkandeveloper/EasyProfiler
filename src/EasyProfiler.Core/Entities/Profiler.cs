e6f3f4a5a8d97b662769c083fa137d13f1189d3c
furkandeveloper <furkan.dvlp@gmail.com>
2020-11-12 01:24:03 +0300

    🔥 Move Entites from EasyProfiler.Entites to EasyProfiler.Core.

---
+++ src/EasyProfiler.Core/Entities/Profiler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyProfiler.Core.Entities
+{
+    /// <summary>
+    /// Profiler entity.
+    /// </summary>
+    public class Profiler : BaseEntity
+    {
+        public string Query { get; set; }
+
+        public long Duration { get; set; }
+
+        public string RequestUrl { get; set; } = "/";
+    }
+}
