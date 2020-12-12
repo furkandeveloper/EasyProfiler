f26f50cd7c9db5292060e270ce6741222427f8d9
furkandeveloper <furkan.dvlp@gmail.com>
2020-12-13 00:41:32 +0300

    🔨 Add QueryType enum for Mongo.

---
+++ src/EasyProfiler.Mongo/Entities/QueryType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace EasyProfiler.Mongo.Models
+{
+    // <summary>
+    /// SQL Query type.
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum QueryType
+    {
+        NONE,
+        SELECT,
+        INSERT,
+        UPDATE,
+        DELETE,
+        OTHER
+    }
+}
