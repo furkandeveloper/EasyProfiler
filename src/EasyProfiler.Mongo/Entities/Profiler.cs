764f3ba0e8b825ba04cf366a9559c220c7562730
furkandeveloper <furkan.dvlp@gmail.com>
2020-12-13 23:24:57 +0300

    🗃 Add profiler entity

--- src/EasyProfiler.Mongo/Entities/Profiler.cs
+++ src/EasyProfiler.Mongo/Entities/Profiler.cs
@@ -1,7 +1,9 @@
-﻿using EasyProfiler.Mongo.Entities;
+﻿using EasyProfiler.Core.Entities;
+using EasyProfiler.Mongo.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using BaseEntity = EasyProfiler.Mongo.Entities.BaseEntity;
 
 namespace EasyProfiler.Mongo.Models
 {
@@ -10,12 +12,24 @@ namespace EasyProfiler.Mongo.Models
     /// </summary>
     public class Profiler : BaseEntity
     {
+        /// <summary>
+        /// Executed query
+        /// </summary>
         public string Query { get; set; }
 
+        /// <summary>
+        /// Duration of executed query
+        /// </summary>
         public long Duration { get; set; }
 
+        /// <summary>
+        /// Request url of executed query
+        /// </summary>
         public string RequestUrl { get; set; } = "/";
 
+        /// <summary>
+        /// Query type of executed query
+        /// </summary>
         public  QueryType QueryType { get; set; }
     }
 }
