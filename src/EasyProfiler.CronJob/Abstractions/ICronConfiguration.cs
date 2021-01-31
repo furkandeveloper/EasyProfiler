2a99dc00211e4d56ada716a0dbfc577ca674fc84
furkandeveloper <furkan.dvlp@gmail.com>
2021-01-31 23:16:00 +0300

    ⛏ Add ICronConfiguration interface.

---
+++ src/EasyProfiler.CronJob/Abstractions/ICronConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyProfiler.CronJob.Abstractions
+{
+    /// <summary>
+    /// This interface includes base parameter for Cron Job.
+    /// </summary>
+    /// <typeparam name="T">
+    /// Cron Job
+    /// </typeparam>
+    public interface ICronConfiguration<T>
+    {
+        /// <summary>
+        /// Cron Expression. For Example; '****' Cron.Minutely
+        /// </summary>
+        public string CronExpression { get; set; }
+
+        /// <summary>
+        /// TimeZone Information
+        /// </summary>
+        public TimeZoneInfo TimeZoneInfo { get; set; }
+    }
+}
