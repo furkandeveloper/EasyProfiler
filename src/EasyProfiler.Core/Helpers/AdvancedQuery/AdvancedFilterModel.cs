9e90e7343ef0a230b12e95bc84f24def4c8a80db
enisn <iletisim@enisnecipoglu.com>
2021-02-23 13:06:44 +0300

    🔥 Remove unnecessary compiler directive

--- src/EasyProfiler.Core/Helpers/AdvancedQuery/AdvancedFilterModel.cs
+++ src/EasyProfiler.Core/Helpers/AdvancedQuery/AdvancedFilterModel.cs
@@ -1,18 +1,13 @@
 ﻿using AutoFilterer.Attributes;
 using AutoFilterer.Types;
 using EasyProfiler.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace EasyProfiler.Core.Helpers.AdvancedQuery
 {
     /// <summary>
     /// Advanced filter model for profiler entity.
     /// </summary>
-#if NET5_0 || NETCOREAPP3_1
     [PossibleSortings("Query", "Duration")]
-#endif
     public class AdvancedFilterModel : PaginationFilterBase
     {
         public AdvancedFilterModel()
