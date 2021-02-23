9405eaf1a7e7151708cac7fddd8f072ac75a8b71
enisn <iletisim@enisnecipoglu.com>
2021-02-23 13:12:15 +0300

    🔥 Remove unnecessary compiler directive in Startup.cs

--- samples/EasyProfiler.Web/Startup.cs
+++ samples/EasyProfiler.Web/Startup.cs
@@ -32,14 +32,11 @@ namespace EasyProfiler.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-#if NET5_0 || NETCOREAPP3_1           
+            services.AddMvc()        
                 .AddJsonOptions(options =>
                 {
                     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-                })
-#endif
-                ;
+                });
             
             services.AddDbContext<SampleDbContext>(options =>
             {
