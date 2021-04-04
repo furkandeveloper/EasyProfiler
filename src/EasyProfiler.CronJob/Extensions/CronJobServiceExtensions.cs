08ced96939fae3eda08fbf512b313c05afb75774
furkandeveloper <furkan.dvlp@gmail.com>
2021-04-04 23:22:48 +0300

    ⛏ Add Cron Job Service Collection Extension

---
+++ src/EasyProfiler.CronJob/Extensions/CronJobServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using EasyProfiler.CronJob.Abstractions;
+using EasyProfiler.CronJob.Common;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyProfiler.CronJob.Extensions
+{
+    public static class CronJobServiceExtensions
+    {
+        public static IServiceCollection ApplyResulation<T>(this IServiceCollection services, Action<ICronConfiguration<T>> action)
+            where T : CronJobService
+        {
+            var options = new CronConfiguration<T>();
+            action.Invoke(options);
+            services.AddSingleton<ICronConfiguration<T>>(options);
+            services.AddHostedService<T>();
+            return services;
+        }
+    }
+}
