ccb30a4b4934da3db3497c0fb65505e9d7794f89
furkandeveloper <furkan.dvlp@gmail.com>
2021-04-04 23:22:33 +0300

    ⛏ Add Db Writer Cron Job

---
+++ src/EasyProfiler.CronJob/Concrete/DbWriterCronJob.cs
@@ -0,0 +1,32 @@
+﻿using EasyProfiler.CronJob.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EasyProfiler.CronJob.Concrete
+{
+    public class DbWriterCronJob : CronJobService
+    {
+        public DbWriterCronJob(ICronConfiguration<DbWriterCronJob> configuration)
+            :base(configuration.CronExpression, configuration.TimeZoneInfo)
+        {
+
+        }
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task DoWork(CancellationToken cancellationToken)
+        {
+            return base.DoWork(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
