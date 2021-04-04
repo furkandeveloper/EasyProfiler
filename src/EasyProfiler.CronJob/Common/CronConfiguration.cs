b5890d21bbdcae34a1f6c7fd8239dd5b69302387
furkandeveloper <furkan.dvlp@gmail.com>
2021-04-04 23:21:26 +0300

    ⛏ Add CronConfiguration object

---
+++ src/EasyProfiler.CronJob/Common/CronConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using EasyProfiler.CronJob.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyProfiler.CronJob.Common
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class CronConfiguration<T> : ICronConfiguration<T>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string CronExpression { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public TimeZoneInfo TimeZoneInfo { get; set; }
+    }
+}
