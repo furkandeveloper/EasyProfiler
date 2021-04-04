9da5e8b6fbf4dd65d884da31f5be157796813140
furkandeveloper <furkan.dvlp@gmail.com>
2021-04-04 23:21:47 +0300

    ⛏ Add resulation enum

---
+++ src/EasyProfiler.CronJob/Common/Resulation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+#if NETCOREAPP3_1 || NET5_0
+using System.Text.Json.Serialization;
+#endif
+
+namespace EasyProfiler.CronJob.Common
+{
+#if NETCOREAPP3_1 || NET5_0
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+#endif
+    public enum Resulation
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        LOW = 1,
+        /// <summary>
+        /// 
+        /// </summary>
+        MEDIUM,
+        /// <summary>
+        /// 
+        /// </summary>
+        HIGH
+    }
+}
