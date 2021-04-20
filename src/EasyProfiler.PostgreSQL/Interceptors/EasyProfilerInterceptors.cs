da9038060e03ef29d1db9cf47c03a57e3db743ea
furkandeveloper <furkan.dvlp@gmail.com>
2021-04-21 00:59:31 +0300

    ⛏ Change interceptor scenario

--- src/EasyProfiler.PostgreSQL/Interceptors/EasyProfilerInterceptors.cs
+++ src/EasyProfiler.PostgreSQL/Interceptors/EasyProfilerInterceptors.cs
@@ -6,6 +6,10 @@ using System.Collections.Generic;
 using System.Data.Common;
 using EasyProfiler.EntityFrameworkCore.Extensions;
 using System;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using EasyProfiler.Core.Statics;
 
 namespace EasyProfiler.PostgreSQL.Interceptors
 {
@@ -15,15 +19,12 @@ namespace EasyProfiler.PostgreSQL.Interceptors
     public class EasyProfilerInterceptors : DbCommandInterceptor
     {
         private readonly IHttpContextAccessor httpContextAccessor;
-        private readonly IMemoryCache memoryCache;
-        private readonly IProfilerCache profilerCache;
 
-        public EasyProfilerInterceptors(IHttpContextAccessor httpContextAccessor, IMemoryCache memoryCache, IProfilerCache profilerCache)
+        public EasyProfilerInterceptors(IHttpContextAccessor httpContextAccessor)
         {
             this.httpContextAccessor = httpContextAccessor;
-            this.memoryCache = memoryCache;
-            this.profilerCache = profilerCache;
         }
+
         /// <summary>
         /// Data reader Disposing.
         /// </summary>
@@ -46,54 +47,8 @@ namespace EasyProfiler.PostgreSQL.Interceptors
                 RequestUrl = httpContextAccessor?.HttpContext?.Request?.Path.Value,
                 QueryType = command.FindQueryType()
             };
-
-            var cachedData = memoryCache.Get<List<Profiler>>("easy-profiler") ?? new List<Profiler>();
-            cachedData.Add(profilerData);
-            memoryCache.Set("easy-profiler", cachedData, TimeSpan.FromDays(1));
-            memoryCache.Set<string>("test", "test-furkan", TimeSpan.FromDays(1));
-
-            profilerCache.Set("easy-profiler", cachedData, TimeSpan.FromDays(1));
-            profilerCache.Set<string>("test", "test-furkan", TimeSpan.FromDays(1));
-            var data = profilerCache.Get<string>("test");
-
-            //cachedValue.Add(new Profiler
-            //{
-            //    Duration = eventData.Duration.Ticks,
-            //    Query = command.CommandText,
-            //    RequestUrl = httpContextAccessor?.HttpContext?.Request?.Path.Value,
-            //    QueryType = command.FindQueryType()
-            //});
-            //easyCacheService.Set<List<Profiler>>("easy-profiler", cachedValue, TimeSpan.FromMinutes(2));
-            //var data = easyCacheService.Get<List<Profiler>>("easy-profiler");
-            //var cachedValue = easyCacheService.GetAndSet("easy-profiler",()=> new List<Profiler> { new Profiler { Query = "Ahmet"} },TimeSpan.FromSeconds(100));
-            //var data = easyCacheService.Get<List<Profiler>>("easy-profiler");
-            //easyCacheService.GetAndSet("test", () => "12", TimeSpan.FromSeconds(100));
-            //var data = easyCacheService.Get<string>("test");
-            //cachedValue=new Profiler
-            //{
-            //    Duration = eventData.Duration.Ticks,
-            //    Query = command.CommandText,
-            //    RequestUrl = httpContextAccessor?.HttpContext?.Request?.Path.Value,
-            //    QueryType = command.FindQueryType()
-            //};
-            //easyCacheService.SetAsync("easy-profiler", cachedValue, TimeSpan.FromMinutes(5)).ConfigureAwait(false);
-            //Task.Run(() => baseService.InsertAsync(new Profiler
-            //{
-            //    Duration = eventData.Duration.Ticks,
-            //    Query = command.CommandText,
-            //    RequestUrl = httpContextAccessor?.HttpContext?.Request?.Path.Value,
-            //    QueryType = command.FindQueryType()
-            //}));
+            Values.Profilers.Add(profilerData);
             return base.DataReaderDisposing(command, eventData, result);
-        }
-
-        private Profiler GetData()
-        {
-            return new Profiler
-            {
-                Query = "Zalım",
-                RequestUrl = httpContextAccessor?.HttpContext?.Request?.Path.Value,
-            };
         }
     }
 }
