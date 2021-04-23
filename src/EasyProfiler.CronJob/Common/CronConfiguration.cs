0894da5fda05dc2eedfb2e131a150d4bf703930d
furkandeveloper <furkan.dvlp@gmail.com>
2021-04-23 17:35:53 +0300

    📝 Add docs for cron configuration object

--- src/EasyProfiler.CronJob/Common/CronConfiguration.cs
+++ src/EasyProfiler.CronJob/Common/CronConfiguration.cs
@@ -6,18 +6,18 @@ using System.Text;
 namespace EasyProfiler.CronJob.Common
 {
     /// <summary>
-    /// 
+    /// Cron Configuration Object
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public class CronConfiguration<T> : ICronConfiguration<T>
     {
         /// <summary>
-        /// 
+        /// Cron Expression. For Example : * * * * *
         /// </summary>
         public string CronExpression { get; set; }
 
         /// <summary>
-        /// 
+        /// Represents any time zone in the world.
         /// </summary>
         public TimeZoneInfo TimeZoneInfo { get; set; }
     }
