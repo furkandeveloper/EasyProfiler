577315ef38a995c398a8ea1ac364d9eee3a30077
furkandeveloper <furkan.dvlp@gmail.com>
2021-04-23 17:37:25 +0300

    📝 Add code comment for resulation cron attribute

--- src/EasyProfiler.CronJob/Common/ResulationCronAttribute.cs
+++ src/EasyProfiler.CronJob/Common/ResulationCronAttribute.cs
@@ -4,14 +4,26 @@ using System.Text;
 
 namespace EasyProfiler.CronJob.Common
 {
+    /// <summary>
+    /// Resulation Cron Attribute
+    /// </summary>
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
     public class ResulationCronAttribute : Attribute
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="cron">
+        /// Cron Expression in string format
+        /// </param>
         public ResulationCronAttribute(string cron)
         {
             this.Cron = cron;
         }
 
+        /// <summary>
+        /// Cron Expression
+        /// </summary>
         public string Cron { get; }
     }
 }
