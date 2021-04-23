9cabe243f6b5f7ba04cdeaec64b023e15827deb1
furkandeveloper <furkan.dvlp@gmail.com>
2021-04-23 18:05:04 +0300

    🗃️ Add database migration

--- src/EasyProfiler.PostgreSQL/Migrations/20210423145154_ChangeDatabaseSchema.Designer.cs
+++ src/EasyProfiler.PostgreSQL/Migrations/20210423145154_ChangeDatabaseSchema.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using EasyProfiler.PostgreSQL.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace EasyProfiler.PostgreSQL.Migrations
 {
     [DbContext(typeof(ProfilerPostgreSqlDbContext))]
-    partial class ProfilerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210423145154_ChangeDatabaseSchema")]
+    partial class ChangeDatabaseSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
