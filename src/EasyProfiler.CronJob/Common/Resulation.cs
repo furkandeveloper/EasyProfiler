6b45e0052f4b9a20221c8fd3ad5e6eeb32366053
furkandeveloper <furkan.dvlp@gmail.com>
2021-04-23 17:36:58 +0300

    📝 Add code comment for resulation enum

--- src/EasyProfiler.CronJob/Common/Resulation.cs
+++ src/EasyProfiler.CronJob/Common/Resulation.cs
@@ -7,26 +7,28 @@ using System.Text.Json.Serialization;
 
 namespace EasyProfiler.CronJob.Common
 {
+    /// <summary>
+    /// Resulation Type
+    /// </summary>
 #if NETCOREAPP3_1 || NET5_0
     [JsonConverter(typeof(JsonStringEnumConverter))]
 #endif
     public enum Resulation
     {
         /// <summary>
-        /// 
+        /// Low Resulation
         /// </summary>
-        //[ResulationCron("*/2 * * * *")]
-        [ResulationCron("* * * * *")]
+        [ResulationCron("*/5 * * * *")]
         LOW = 1,
         /// <summary>
-        /// 
+        /// Mid. Resulation
         /// </summary>
         [ResulationCron("*/2 * * * *")]
         MEDIUM,
         /// <summary>
-        /// 
+        /// High Resulation
         /// </summary>
-        [ResulationCron("*/2 * * * *")]
+        [ResulationCron("* * * * *")]
         HIGH
     }
 }
