a64a0f58df328fa8be28202bd839283a702f913a
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-01 20:32:25 +0300

    ⛏ Create BaseException

---
+++ src/EasyProfiler.Core/Exceptions/BaseException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyProfiler.Core.Exceptions
+{
+    public class BaseException : Exception
+    {
+        public BaseException(string message) : base(message)
+        {
+        }
+    }
+}
