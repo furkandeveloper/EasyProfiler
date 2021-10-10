34c54868845e145737c1af2139e56dc82d3fb32f
Furkan Güngör <furkan.dvlp@gmail.com>
2021-10-11 00:28:27 +0300

    ⛏ Add sample endpoint

---
+++ src/EasyProfiler.AspNetCore/Controllers/ProfilerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyProfiler.AspNetCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProfilerController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetProfilerAsync()
+        {
+            return Ok("Hello Easy Profiler");
+        }
+    }
+}
