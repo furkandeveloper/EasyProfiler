050a1fedc5591a8db310164807253571d5ce49d1
Furkan Güngör <furkan.dvlp@gmail.com>
2021-10-11 00:28:11 +0300

    ⛏ Implement IMvcBuilder extension

---
+++ src/EasyProfiler.AspNetCore/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyProfiler.AspNetCore
+{
+    public static class Startup
+    {
+        public static IMvcBuilder AddEasyProfilerControllers(this IMvcBuilder builder)
+        {
+            builder.AddApplicationPart(Assembly.GetExecutingAssembly());
+            return builder;
+        }
+    }
+}
