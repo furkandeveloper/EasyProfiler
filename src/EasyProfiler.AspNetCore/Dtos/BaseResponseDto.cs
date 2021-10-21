7c509f4d9e22fa1a4bafe1e31b958b3b7699e62b
Furkan Güngör <furkan.dvlp@gmail.com>
2021-10-21 22:41:22 +0300

    ⛏ Add base response data transfer object

---
+++ src/EasyProfiler.AspNetCore/Dtos/BaseResponseDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyProfiler.AspNetCore.Dtos
+{
+    /// <summary>
+    /// Base Response Data Transfer Object
+    /// </summary>
+    internal class BaseResponseDto
+    {
+        /// <summary>
+        /// PK
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Start Date
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// End Date
+        /// </summary>
+        public DateTime EndDate { get; set; }
+    }
+}
