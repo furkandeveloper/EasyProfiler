2e39923d5c0d536303f9c195150ea343ee2cce00
Furkan Güngör <furkan.dvlp@gmail.com>
2022-02-26 21:31:37 +0300

    🔨 Add Host extension method to the SQL Server implementation

---
+++ src/EasyProfiler.PostgreSQL/Extensions/Host.cs
@@ -0,0 +1,41 @@
+﻿using EasyProfiler.PostgreSQL.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+
+namespace EasyProfiler.PostgreSQL.Extensions
+{
+    /// <summary>
+    /// This class includes <see cref="IHost"/> extensions for database migrations.
+    /// Implements database migration.
+    /// </summary>
+    public static class Host
+    {
+        /// <summary>
+        /// This method provides database migration for PostgreSql. 
+        /// </summary>
+        /// <param name="host">
+        /// <see cref="IHost"/>
+        /// </param>
+        /// <returns>
+        /// Returns a <see cref="IHost"/>
+        /// </returns>
+        public static IHost MigrateEasyProfiler(this IHost host)
+        {
+            using var serviceScope = host.Services.CreateScope();
+            var context = serviceScope.ServiceProvider.GetService<ProfilerPostgreSqlDbContext>();
+            var pendingMigrations = context.Database.GetPendingMigrations().ToArray();
+            if (pendingMigrations.Length > 0)
+            {
+                context.Database.Migrate();
+            }
+            else
+            {
+                context.Database.EnsureCreated();
+            }
+
+            return host;
+        }
+    }
+}
