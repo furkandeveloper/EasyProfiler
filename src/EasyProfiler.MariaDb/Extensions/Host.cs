4314ea46f29b3877ff8d8028cc47c2844a0df831
Furkan Güngör <furkan.dvlp@gmail.com>
2022-02-26 21:22:30 +0300

    🔨 Add host builder extension to the mariadb implementation

---
+++ src/EasyProfiler.MariaDb/Extensions/Host.cs
@@ -0,0 +1,41 @@
+﻿using EasyProfiler.MariaDb.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+
+namespace EasyProfiler.MariaDb.Extensions
+{
+    /// <summary>
+    /// This class includes <see cref="IHost"/> extensions for database migrations.
+    /// Implements database migration.
+    /// </summary>
+    public static class Host
+    {
+        /// <summary>
+        /// This method provides database migration for Maria&MySql. 
+        /// </summary>
+        /// <param name="host">
+        /// <see cref="IHost"/>
+        /// </param>
+        /// <returns>
+        /// Returns a <see cref="IHost"/>
+        /// </returns>
+        public static IHost MigrateEasyProfiler(this IHost host)
+        {
+            using var serviceScope = host.Services.CreateScope();
+            var context = serviceScope.ServiceProvider.GetService<ProfilerMariaDbContext>();
+            var pendingMigrations = context.Database.GetPendingMigrations().ToArray();
+            if (pendingMigrations.Length > 0)
+            {
+                context.Database.Migrate();
+            }
+            else
+            {
+                context.Database.EnsureCreated();
+            }
+
+            return host;
+        }
+    }
+}
