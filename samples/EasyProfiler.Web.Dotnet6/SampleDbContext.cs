46566e60f9897e2fd2d1464f5b58409c5b1888ec
furkandeveloper <furkan.dvlp@gmail.com>
2022-03-12 23:41:23 +0300

    🔨 Add Sample Db Context to the dotnet6 project

---
+++ samples/EasyProfiler.Web.Dotnet6/SampleDbContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EasyProfiler.Web.Dotnet6
+{
+    /// <summary>
+    /// Sample Db Context
+    /// </summary>
+    public class SampleDbContext : DbContext
+    {
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="options">
+        /// DbContextOptions
+        /// </param>
+        public SampleDbContext(DbContextOptions<SampleDbContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        protected SampleDbContext()
+        {
+        }
+
+        /// <summary>
+        /// Customer Table
+        /// </summary>
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        /// <summary>
+        /// OnModelCreating
+        /// </summary>
+        /// <param name="modelBuilder">
+        /// Summary:
+        ///     Provides a simple API surface for configuring a Microsoft.EntityFrameworkCore.Metadata.IMutableModel
+        ///     that defines the shape of your entities, the relationships between them, and
+        ///     how they map to the database.
+        ///     You can use Microsoft.EntityFrameworkCore.ModelBuilder to construct a model for
+        ///     a context by overriding Microsoft.EntityFrameworkCore.DbContext.OnModelCreating(Microsoft.EntityFrameworkCore.ModelBuilder)
+        ///     on your derived context. Alternatively you can create the model externally and
+        ///     set it on a Microsoft.EntityFrameworkCore.DbContextOptions instance that is passed
+        ///     to the context constructor.
+        /// </param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity
+                    .Property(p => p.CustomerId)
+                    .UseIdentityColumn();
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
